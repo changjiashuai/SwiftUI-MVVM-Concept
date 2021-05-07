664d379c3ed82916f44ad738b5f40d5aac420b68
Igor Shelopaev <igorshelopaev@gmail.com>
2021-05-07 13:29:20 +0300

    changes

---
+++ Shared/view/protocol/AbstractToolBar.swift
@@ -0,0 +1,15 @@
+//
+//  AbstractToolBar.swift
+//  UniversalComponent
+//
+//  Created by Igor Shelopaev on 07.05.2021.
+//
+
+import SwiftUI
+
+
+/// Protocol for tool bars
+protocol AbstractToolBar: View {
+    var title: String? { get }
+}
+

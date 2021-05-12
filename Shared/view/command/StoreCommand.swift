1ccfcfea1d8984e61230e26341c578a758ab89e8
Igor Shelopaev <igorshelopaev@gmail.com>
2021-05-12 11:59:40 +0300

    changes

--- Shared/view/command/StoreCommand.swift
+++ Shared/view/command/StoreCommand.swift
@@ -9,14 +9,11 @@ import Foundation
 
 
 /// Defines command for store from a UI control
-class StoreCommand: Command, Equatable {
+class StoreCommand: Equatable {
     
     /// Time stamp to differ repeating the same command several times in a row
     let date = Date()
-    
-    /// execute method
-    func execute(){}
-    
+       
     /// execute method
     func execute<V:Model, U: Proxy>(store : RemoteStore<V,U>){}
     

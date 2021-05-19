37b66e44621865da590500270a4b1fdede0c898e
Igor Shelopaev <igorshelopaev@gmail.com>
2021-05-19 10:50:44 +0300

    changes

---
+++ Shared/model/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  UniversalComponent
+//
+//  Created by Igor Shelopaev on 19.05.2021.
+//
+
+import Foundation
+import Data
+
+/// Model defining Movie
+struct Movie: Model {
+    
+    // Book ID
+    let id: Int
+    
+    /// Title of a movie
+    let title: String
+
+}

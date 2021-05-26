1a24e3d523910542cad7057a81ba71f01cdc7735
Igor Shelopaev <igorshelopaev@gmail.com>
2021-05-26 10:48:38 +0300

    changes

--- Shared/view/components/StatusBar.swift
+++ Shared/view/components/StatusBar.swift
@@ -18,7 +18,7 @@ struct StatusBar: View, BlueStylable  {
     /// The type of view representing the body of this view
     var body: some View {
         HStack{
-            Text("Total intems: \(total)").foregroundColor(.white).font(.system(size: 9, weight: .thin))
+            Text("Total items: \(total)").foregroundColor(.white).font(.system(size: 9, weight: .thin))
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(5)

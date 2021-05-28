2b9f2424646efe0cac29320108d23fbee3eef92d
Igor Shelopaev <igorshelopaev@gmail.com>
2021-05-28 10:19:47 +0300

    changes

--- Packages/Ui/Sources/Ui/toolbar/command/protocol/Controllable.swift
+++ Packages/Ui/Sources/Ui/toolbar/command/protocol/Controllable.swift
@@ -10,31 +10,35 @@ import Data
 import Service
 
 /// Add controllable behavior to view to obey commands from others views
-public protocol Controllable{
-    
+public protocol Controllable {
+
     associatedtype Item: Model
     associatedtype AbstractProxy: Proxy
     associatedtype AbstractAuthentication: AuthenticationAPI
-    
+
     /// Store with data
-    var store: RemoteStore<Item, AbstractProxy> {get}
-    
+    var store: RemoteStore<Item, AbstractProxy> { get }
+
     /// Authentication service
-    var authentication: AbstractAuthentication  {get}
+    var authentication: AbstractAuthentication { get }
 }
 
-extension Controllable{
-    
+extension Controllable {
+
     /// Act on a command from the ToolBar
     /// - Parameter command: Command from toolBar to do some actions
     public func onCommandChanged(_ command: StoreCommand) {
         command.execute(store: store)
     }
-    
+
     /// load data
     public func load() {
         ///Using authentication service generate secret token to prove level of access to communicate with remote sources
-        store.load(params: ["page": "*", "access token": authentication.getToken()]) {
+        store.load(
+            params: ["page": "*",
+                "access token": authentication.getToken()
+            ]
+        ) {
             print("🟦 do something after loading")
         }
     }
--- Shared/view/components/Master.swift
+++ Shared/view/components/Master.swift
@@ -21,28 +21,28 @@ import Ui
 struct Master<T: Model, D: Model, V: Proxy, U: Proxy, F: View, Content: View>:
     View, Controllable, Loadable, Selectable, BlueStylable, Componentable, Scrolable
 {
-    
+
     /// Store with data
     @StateObject var store: RemoteStore<T, U>
-    
+
     /// Store with data
     @StateObject var detail: RemoteStore<D, V>
-    
+
     /// Authentication service
     @EnvironmentObject var authentication: Authentication
-    
+
     /// Selected item
     @State var selectedItem: T?
-    
+
     /// A view builder that creates the content of an Item view
     let content: (T, Bool) -> Content
-    
+
     /// ToolBar with set of controls
     let toolBar: F
-    
+
     /// Indicates master Store loading
     @Binding var masterIsLoading: Bool
-    
+
     /// The type of view representing the body of this view
     var body: some View {
         VStack(spacing: 0) {
@@ -50,13 +50,13 @@ struct Master<T: Model, D: Model, V: Proxy, U: Proxy, F: View, Content: View>:
             controlRender()
             StatusBar(total: $store.total)
         }
-        .mask(!notLoading)
-        .onReceive(store.$loading, perform: onLoadingChange)
-        .onAppear { if notLoading { load(); detail.removeAll() } }
+            .mask(!notLoading)
+            .onReceive(store.$loading, perform: onLoadingChange)
+            .onAppear { if notLoading { load(); detail.removeAll() } }
     }
-    
+
     // MARK: - API Methods
-    
+
     /// Get list View
     /// - Returns: list view
     @ViewBuilder
@@ -68,25 +68,32 @@ struct Master<T: Model, D: Model, V: Proxy, U: Proxy, F: View, Content: View>:
             }
         }
     }
-    
+
     /// Select item
     /// - Parameter item: selected item
     func select(_ item: T) {
         selectedItem = item
         loadDetails(item)
     }
-    
+
     // MARK: - Private Methods
-    
+
     /// Process master Store loading
     /// - Parameter loading: loading indicator
-    private func onLoadingChange(_ loading: Bool){
+    private func onLoadingChange(_ loading: Bool) {
         masterIsLoading = loading
     }
-    
+
     /// Load detail store for a master item
     /// - Parameter item: current master item
-    private func loadDetails(_ item: T){
-        detail.load(params: ["page": "*", "access token": authentication.getToken(), "masterId": "\(item.id)"], callback: {masterIsLoading = false; print("🟦 do something") })
+    private func loadDetails(_ item: T) {
+        detail.load(
+            params: [
+                "page": "*",
+                "access token": authentication.getToken(),
+                "masterId": "\(item.id)"
+            ],
+            callback: { masterIsLoading = false; print("🟦 do something") }
+        )
     }
 }
